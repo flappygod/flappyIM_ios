bfd4b7b72cbf0f3f4491b833cb67900f03f60d74
lijunlin <327603258@qq.com>
2019-08-08 17:20:58 +0800

    增加数据库更新的方法

--- flappyim/Classes/Public/DataBase.m
+++ flappyim/Classes/Public/DataBase.m
@@ -40,16 +40,9 @@
 
 //初始化数据库
 -(void)setup{
-    //1.创建database路径
-    NSString *docuPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
-    NSString *dbPath = [docuPath stringByAppendingPathComponent:@"flappyim.db"];
-    NSLog(@"!!!dbPath = %@",dbPath);
-    //2.创建对应路径下数据库
-    FMDatabase* db = [FMDatabase databaseWithPath:dbPath];
-    //3.在数据库中进行增删改查操作时，需要判断数据库是否open，如果open失败，可能是权限或者资源不足，数据库操作完成通常使用close关闭数据库
-    [db open];
-    if (![db open]) {
-        NSLog(@"db open fail");
+    //获取db
+    FMDatabase* db=[self openDB];
+    if(db==nil){
         return;
     }
     //4.数据库中创建表（可创建多张）
@@ -63,12 +56,8 @@
     [db close];
 }
 
-
-
-
-//插入消息
--(Boolean)insert:(ChatMessage*)msg{
-    
+//获取db
+-(FMDatabase*)openDB{
     //1.创建database路径
     NSString *docuPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
     NSString *dbPath = [docuPath stringByAppendingPathComponent:@"flappyim.db"];
@@ -79,6 +68,19 @@
     [db open];
     if (![db open]) {
         NSLog(@"db open fail");
+        return nil;
+    }
+    return db;
+}
+
+
+
+//插入消息
+-(Boolean)insert:(ChatMessage*)msg{
+
+    //获取db
+    FMDatabase* db=[self openDB];
+    if(db==nil){
         return false;
     }
     BOOL result = [db executeUpdate:@"insert into 'message'(messageId,messageSession,messageSessionType,messageSessionOffset,messageTableSeq,messageType,messageSend,messageRecieve,messageContent,messageSended,messageReaded,messageDate,messageDeletedDate,messageDeleted) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)" withArgumentsInArray:@[msg.messageId,msg.messageSession,[NSNumber numberWithInteger:msg.messageSessionType],[NSNumber numberWithInteger:msg.messageSessionOffset],[NSNumber numberWithInteger:msg.messageTableSeq],[NSNumber numberWithInteger:msg.messageType],msg.messageSend,msg.messageRecieve,msg.messageContent,[NSNumber numberWithInteger:msg.messageSended],[NSNumber numberWithInteger:msg.messageReaded],msg.messageDate,msg.messageDeletedDate,[NSNumber numberWithInteger:msg.messageDeleted]]];
@@ -90,6 +92,56 @@
     }
 }
 
+
+
+//通过ID获取消息
+-(ChatMessage*)getMessageByID:(NSString*)messageID{
+    //获取db
+    FMDatabase* db=[self openDB];
+    if(db==nil){
+        return nil;
+    }
+    FMResultSet *result = [db executeQuery:@"select * from 'message' where messageId = ?" withArgumentsInArray:@[messageID]];
+    //返回消息
+    while ([result next]) {
+        ChatMessage *msg = [ChatMessage new];
+        msg.messageId = [result stringForColumn:@"messageId"];
+        msg.messageSession = [result stringForColumn:@"messageSession"];
+        msg.messageSessionType = [result intForColumn:@"messageSessionType"];
+        msg.messageSessionOffset = [result intForColumn:@"messageSessionOffset"];
+        msg.messageTableSeq = [result intForColumn:@"messageTableSeq"];
+        msg.messageType = [result intForColumn:@"messageType"];
+        msg.messageSend = [result stringForColumn:@"messageSend"];
+        msg.messageRecieve = [result stringForColumn:@"messageRecieve"];
+        msg.messageContent = [result stringForColumn:@"messageContent"];
+        msg.messageSended = [result intForColumn:@"messageSended"];
+        msg.messageReaded = [result intForColumn:@"messageReaded"];
+        msg.messageDate = [result stringForColumn:@"messageDate"];
+        msg.messageDeleted = [result intForColumn:@"messageDeleted"];
+        msg.messageDeletedDate = [result stringForColumn:@"messageDeletedDate"];
+        [db close];
+        //返回消息
+        return msg;
+    }
+    [db close];
+    return nil;
+}
+
+//更新数据
+-(Boolean)updateMessage:(ChatMessage*)msg{
+    //获取db
+    FMDatabase* db=[self openDB];
+    if(db==nil){
+        return false;
+    }
+    BOOL result = [db executeUpdate:@"update 'message'(messageId,messageSession,messageSessionType,messageSessionOffset,messageTableSeq,messageType,messageSend,messageRecieve,messageContent,messageSended,messageReaded,messageDate,messageDeletedDate,messageDeleted) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?) where messageId = ?" withArgumentsInArray:@[msg.messageSession,[NSNumber numberWithInteger:msg.messageSessionType],[NSNumber numberWithInteger:msg.messageSessionOffset],[NSNumber numberWithInteger:msg.messageTableSeq],[NSNumber numberWithInteger:msg.messageType],msg.messageSend,msg.messageRecieve,msg.messageContent,[NSNumber numberWithInteger:msg.messageSended],[NSNumber numberWithInteger:msg.messageReaded],msg.messageDate,msg.messageDeletedDate,[NSNumber numberWithInteger:msg.messageDeleted],msg.messageId]];
+    [db close];
+    if (result) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 
