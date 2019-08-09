0bc81eca76ca2026eba56b664701023ca94ffc1a
lijunlin <327603258@qq.com>
2019-08-09 10:31:37 +0800

    微信

---
+++ flappyim/Classes/Model/FlappySession.h
@@ -0,0 +1,22 @@
+//
+//  FlappySession.h
+//  AFNetworking
+//
+//  Created by lijunlin on 2019/8/9.
+//
+
+#import <Foundation/Foundation.h>
+#import "PostTool.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlappySession : NSObject
+
+
+//设置消息的监听
+-(void)setMessageListener:(MessageListener*)listener;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Model/FlappySession.m
@@ -0,0 +1,22 @@
+//
+//  FlappySession.m
+//  AFNetworking
+//
+//  Created by lijunlin on 2019/8/9.
+//
+
+#import "FlappySession.h"
+
+@implementation FlappySession
+{
+    //消息
+    MessageListener* _listener;
+}
+
+
+//设置消息的监听
+-(void)setMessageListener:(MessageListener*)listener{
+    
+}
+
+@end
