1fe644aa9fc07fc880b8b6499a9eb9dfb0e330ba
lijunlin <327603258@qq.com>
2019-08-12 12:39:25 +0800

    1

--- flappyim/Classes/Public/FlappyIM.m
+++ flappyim/Classes/Public/FlappyIM.m
@@ -682,12 +682,10 @@
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err{
     //非正常d退出
     [self offline:false];
-    //在主线程之中执行
+    //退出了
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         dispatch_async(dispatch_get_main_queue(), ^{
-            
             [[FlappySender shareInstance] failureCallbacks];
-            
         });
     });
 }
