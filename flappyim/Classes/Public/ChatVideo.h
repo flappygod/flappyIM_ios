e83f8885a543acae3c5af6c4382fd53badbefe0b
lijunlin <327603258@qq.com>
2019-08-22 21:43:53 +0800

    增加发送视频

---
+++ flappyim/Classes/Public/ChatVideo.h
@@ -0,0 +1,30 @@
+//
+//  ChatVideo.h
+//  AFNetworking
+//
+//  Created by lijunlin on 2019/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatVideo : NSObject
+
+//发送的本地地址
+@property(nonatomic,copy) NSString*  sendPath;
+//地址
+@property(nonatomic,copy) NSString*  path;
+//封面图片
+@property(nonatomic,copy) NSString*  coverPath;
+//时长
+@property(nonatomic,copy) NSString*  duration;
+//宽度
+@property(nonatomic,copy) NSString*  width;
+//高度
+@property(nonatomic,copy) NSString*  height;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Public/ChatVideo.m
@@ -0,0 +1,12 @@
+//
+//  ChatVideo.m
+//  AFNetworking
+//
+//  Created by lijunlin on 2019/8/22.
+//
+
+#import "ChatVideo.h"
+
+@implementation ChatVideo
+
+@end
