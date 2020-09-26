0e0437f59213c8ef5ff722d5e6d8f7904f4b9918
lijunlin <327603258@qq.com>
2020-09-26 15:50:23 +0800

    增加注释

--- flappyim/Classes/Config/FlappyApiConfig.h
+++ flappyim/Classes/Config/FlappyApiConfig.h
@@ -14,35 +14,47 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)shareInstance;
 
+
+//设备
+@property (nonatomic,copy) NSString*  pushPlat;
+//心跳间隔
+@property (nonatomic,assign) NSInteger  heartInterval;
+
+
+
 //基础地址
 @property(nonatomic,copy) NSString* BaseUrl;
 //基础地址
 @property(nonatomic,copy) NSString* BaseUploadUrl;
-
 //上传文件的地址
 @property(nonatomic,copy) NSString* URL_fileUpload;
+//视频上传
 @property(nonatomic,copy) NSString* URL_videoUpload;
-
-
+//修改推送
 @property(nonatomic,copy) NSString* URL_changePush;
+//注册
 @property(nonatomic,copy) NSString* URL_register;
+//登录
 @property(nonatomic,copy) NSString* URL_login;
+//登出
 @property(nonatomic,copy) NSString* URL_logout;
+//自动登录
 @property(nonatomic,copy) NSString* URL_autoLogin;
+//创建单聊
 @property(nonatomic,copy) NSString* URL_createSingleSession;
+//获取单聊
 @property(nonatomic,copy) NSString* URL_getSingleSession;
+//创建群聊
 @property(nonatomic,copy) NSString* URL_createGroupSession;
+//获取会话
 @property(nonatomic,copy) NSString* URL_getSessionByExtendID;
+//获取所有会话
 @property(nonatomic,copy) NSString* URL_getUserSessions;
+//添加用户到会话
 @property(nonatomic,copy) NSString* URL_addUserToSession;
+//删除会话中的用户
 @property(nonatomic,copy) NSString* URL_delUserInSession;
 
-
-//设备
-@property (nonatomic,copy) NSString*  pushPlat;
-
-//心跳间隔
-@property (nonatomic,assign) NSInteger  heartInterval;
 
 
 //重新设置服务器地址
