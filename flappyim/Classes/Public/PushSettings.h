9d0c6a12b02c0cebdf571afb3a68e64f8b9f1138
lilin <327603258@qq.com>
2024-04-02 15:02:03 +0800

    增加推送设置的获取和设置

---
+++ flappyim/Classes/Public/PushSettings.h
@@ -0,0 +1,16 @@
+//
+//  PushSettings.h
+//  flappyim
+//
+//  Created by li lin on 2024/4/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PushSettings : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Public/PushSettings.m
@@ -0,0 +1,12 @@
+//
+//  PushSettings.m
+//  flappyim
+//
+//  Created by li lin on 2024/4/2.
+//
+
+#import "PushSettings.h"
+
+@implementation PushSettings
+
+@end
