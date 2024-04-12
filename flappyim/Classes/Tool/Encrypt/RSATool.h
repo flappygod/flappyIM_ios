162c805c6df9ef91d891f939e657cf74bf7d035e
lilin <327603258@qq.com>
2024-04-12 14:50:01 +0800

    添加RSA加密工具类

---
+++ flappyim/Classes/Tool/Encrypt/RSATool.h
@@ -0,0 +1,16 @@
+//
+//  RSATool.h
+//  AFNetworking
+//
+//  Created by li lin on 2024/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSATool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Tool/Encrypt/RSATool.m
@@ -0,0 +1,12 @@
+//
+//  RSATool.m
+//  AFNetworking
+//
+//  Created by li lin on 2024/4/12.
+//
+
+#import "RSATool.h"
+
+@implementation RSATool
+
+@end
