16a82b6f492b3459c81c3ee30719b6a4723f72e1
lilin <327603258@qq.com>
2024-04-18 11:38:18 +0800

    Change IM Database session members

---
+++ flappyim/Classes/Public/SessionDataMember.h
@@ -0,0 +1,16 @@
+//
+//  SessionDataMember.h
+//  flappyim
+//
+//  Created by li lin on 2024/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SessionDataMember : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Public/SessionDataMember.m
@@ -0,0 +1,12 @@
+//
+//  SessionDataMember.m
+//  flappyim
+//
+//  Created by li lin on 2024/4/18.
+//
+
+#import "SessionDataMember.h"
+
+@implementation SessionDataMember
+
+@end
