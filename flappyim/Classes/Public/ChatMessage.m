ce27836d007680ec085b013baa9e4a9a9532b2b5
lilin <327603258@qq.com>
2024-04-23 19:35:26 +0800

    增加消息加密逻辑

--- flappyim/Classes/Public/ChatMessage.m
+++ flappyim/Classes/Public/ChatMessage.m
@@ -5,25 +5,41 @@
 //  Created by lijunlin on 2019/8/8.
 //
 
+#import "FlappyStringTool.h"
+#import "FlappyJsonTool.h"
 #import "ChatMessage.h"
 #import "MJExtension.h"
-#import "FlappyJsonTool.h"
+#import "Aes128.h"
 
 @implementation ChatMessage
 
 
-//设置|获取消息
+//设置|获取系统消息
 -(void)setChatSystem:(ChatSystem*)chatSystem{
     if(chatSystem!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatSystem mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatSystem mj_keyValues]];
+        _messageContent = [self encrypt:content withSecret:nil];
     }
 }
 -(ChatSystem*)getChatSystem{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_SYSTEM){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatSystem mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatSystem mj_objectWithKeyValues:dic];
+    }
+    return nil;
+}
+
+//设置|获取动作
+-(void)setChatAction:(ChatAction*)chatAction{
+    if(chatAction!=nil){
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatAction mj_keyValues]];
+        _messageContent = [self encrypt:content withSecret:nil];
+    }
+}
+-(ChatAction*)getChatAction{
+    if(_messageContent!=nil&&self.messageType==MSG_TYPE_ACTION){
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatAction mj_objectWithKeyValues:dic];
     }
     return nil;
 }
@@ -32,12 +48,12 @@
 //设置|获取文本
 -(void)setChatText:(NSString*)chatText{
     if(chatText!=nil){
-        _messageContent=[self base64EncodeString:chatText];
+        _messageContent=[self encrypt:chatText withSecret: [FlappyStringTool RandomString:16]];
     }
 }
 -(NSString*)getChatText{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_TEXT){
-        return [self base64DecodeString:_messageContent];
+        return [self decrypt];
     }
     return nil;
 }
@@ -46,15 +62,14 @@
 //设置|获取图像
 -(void)setChatImage:(ChatImage*)chatImage{
     if(chatImage!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatImage mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatImage mj_keyValues]];
+        _messageContent=[self encrypt:content withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(ChatImage*)getChatImage{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_IMG){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatImage mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatImage mj_objectWithKeyValues:dic];
     }
     return nil;
 }
@@ -62,15 +77,14 @@
 //设置|获取声音
 -(void)setChatVoice:(ChatVoice*)chatVoice{
     if(chatVoice!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatVoice mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatVoice mj_keyValues]];
+        _messageContent=[self encrypt:content withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(ChatVoice*)getChatVoice{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_VOICE){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatVoice mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatVoice mj_objectWithKeyValues:dic];
     }
     return nil;
 }
@@ -78,15 +92,14 @@
 //设置|获取视频
 -(void)setChatVideo:(ChatVideo*)chatVideo{
     if(chatVideo!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatVideo mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatVideo mj_keyValues]];
+        _messageContent=[self encrypt:content withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(ChatVideo*)getChatVideo{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_VIDEO){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatVideo mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatVideo mj_objectWithKeyValues:dic];
     }
     return nil;
 }
@@ -94,15 +107,14 @@
 //设置|获取位置
 -(void)setChatLocation:(ChatLocation*)chatLocation{
     if(chatLocation!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatLocation mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatLocation mj_keyValues]];
+        _messageContent=[self encrypt:content withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(ChatLocation*)getChatLocation{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_LOCATE){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatLocation mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatLocation mj_objectWithKeyValues:dic];
     }
     return nil;
 }
@@ -111,63 +123,71 @@
 //设置|获取文件
 -(void)setChatFile:(ChatFile*)chatFile{
     if(chatFile!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatFile mj_keyValues]]];
+        NSString* content = [FlappyJsonTool DicToJSONString:[chatFile mj_keyValues]];
+        _messageContent=[self encrypt:content withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(ChatFile*)getChatFile{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_FILE){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatFile mj_objectWithKeyValues:dic];
-        }
+        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self decrypt]];
+        return [ChatFile mj_objectWithKeyValues:dic];
     }
     return nil;
 }
 
-//设置|获取动作
--(void)setChatAction:(ChatAction*)chatAction{
-    if(chatAction!=nil){
-        _messageContent=[self base64EncodeString:[FlappyJsonTool DicToJSONString:[chatAction mj_keyValues]]];
-    }
-}
--(ChatAction*)getChatAction{
-    if(_messageContent!=nil&&self.messageType==MSG_TYPE_ACTION){
-        NSDictionary* dic=[FlappyJsonTool JSONStringToDictionary:[self base64DecodeString:_messageContent]];
-        if(dic!=nil){
-            return [ChatAction mj_objectWithKeyValues:dic];
-        }
-    }
-    return nil;
-}
 
 //设置|获取自定义
 -(void)setChatCustom:(NSString*)chatText{
     if(chatText!=nil){
-        _messageContent=[self base64EncodeString:chatText];
+        _messageContent=[self encrypt:chatText withSecret:[FlappyStringTool RandomString:16]];
     }
 }
 -(NSString*)getChatCustom{
     if(_messageContent!=nil&&self.messageType==MSG_TYPE_CUSTOM){
-        return [self base64DecodeString:_messageContent];
+        return [self decrypt];
     }
     return nil;
 }
 
+//加密
+-(NSString*)encrypt:(NSString*) data withSecret:(NSString*)secret{
+    _messageSecretSend = secret;
+    if(_messageSecretSend==nil||_messageSecretSend.length==0){
+        return [self base64Encode:data];
+    }else{
+        return [Aes128 AES128Encrypt:data withKey:secret];
+    }
+}
 
-#pragma mark -对一个字符串进行base64编码，并返回
--(NSString *)base64EncodeString:(NSString *)string{
+//解密
+-(NSString*)decrypt{
+    NSString* secret = _messageSecretReceive;
+    if(secret==nil||secret.length==0){
+        secret = _messageSecretSend;
+    }
+    if(secret==nil||secret.length==0){
+        return [self base64Decode:_messageContent];
+    }else{
+        return  [Aes128 AES128Decrypt:_messageContent withKey:secret];
+    }
+}
+
+//Base64加密
+-(NSString *)base64Encode:(NSString *)string{
     //1、先转换成二进制数据
     NSData *data =[string dataUsingEncoding:NSUTF8StringEncoding];
     //2、对二进制数据进行base64编码，完成后返回字符串
     return [[data base64EncodedStringWithOptions:0] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
 }
-#pragma mark -对一个字符串进行base64解码，并返回
--(NSString *)base64DecodeString:(NSString *)string{
+
+//Base64解码
+-(NSString *)base64Decode:(NSString *)string{
     //注意：该字符串是base64编码后的字符串
     //1、转换为二进制数据（完成了解码的过程）
     NSData *data=[[NSData alloc]initWithBase64EncodedString:[string stringByReplacingOccurrencesOfString:@"\n" withString:@""] options:0];
     //2、把二进制数据转换成字符串
     return [[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
 }
+
 
 @end
