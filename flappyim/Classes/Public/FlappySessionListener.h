6262375cfd0e0f0bb4683d39fc9498b98172f683
lilin <327603258@qq.com>
2024-08-19 16:49:28 +0800

    change session update and session list update notify function

---
+++ flappyim/Classes/Public/FlappySessionListener.h
@@ -0,0 +1,16 @@
+//
+//  FlappySessionListener.h
+//  flappyim
+//
+//  Created by li lin on 2024/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlappySessionListener : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Public/FlappySessionListener.m
@@ -0,0 +1,12 @@
+//
+//  FlappySessionListener.m
+//  flappyim
+//
+//  Created by li lin on 2024/8/19.
+//
+
+#import "FlappySessionListener.h"
+
+@implementation FlappySessionListener
+
+@end
