a912ea4aab69f5b5775a731ff979d0ed41baabc3
lijunlin <327603258@qq.com>
2025-12-08 17:28:31 +0800

    拆分回执消息类型

---
+++ flappyim/Classes/Public/ChatReadReceipt.h
@@ -0,0 +1,16 @@
+//
+//  ChatReadReceipt.h
+//  flappyim
+//
+//  Created by Lijunlin on 2025/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatReadReceipt : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flappyim/Classes/Public/ChatReadReceipt.m
@@ -0,0 +1,12 @@
+//
+//  ChatReadReceipt.m
+//  flappyim
+//
+//  Created by Lijunlin on 2025/12/8.
+//
+
+#import "ChatReadReceipt.h"
+
+@implementation ChatReadReceipt
+
+@end
